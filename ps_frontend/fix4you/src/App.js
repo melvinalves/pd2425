3f8771f7459b1dd0d8bb0dd24a4ffa9fba8a1396
melvinalves <57185031+melvinalves@users.noreply.github.com>
2024-10-14 23:02:19 +0100

    Update App.js

--- ps_frontend/fix4you/src/App.js
+++ ps_frontend/fix4you/src/App.js
@@ -31,9 +31,6 @@ function App() {
         <footer className="text-center bg-gray-800 text-white p-8">
           <p>© 2024 Fix4You, Inc. All rights reserved.</p>
           <div className="mt-4">
-            <a href="#" className="text-yellow-500 mx-2 hover:underline">Contact Us</a> |
-            <a href="#" className="text-yellow-500 mx-2 hover:underline">About</a> |
-            <a href="#" className="text-yellow-500 mx-2 hover:underline">FAQs</a>
           </div>
         </footer>
       </div>
